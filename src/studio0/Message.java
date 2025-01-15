86b167265dea6a54293d499ce336b16ff037da41
raina <raina@localhost>
2025-01-15 15:26:30 -0600

    go team!

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -1,6 +1,6 @@
 package studio0;
 
-public class Message {
+public class Message { //hi this is raina!
 
 	public static void main(String args[]) {
 		System.out.println(" theasdl;fk   l;k j workshop! ");
